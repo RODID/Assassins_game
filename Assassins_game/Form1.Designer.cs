d1206717596c0bf3a8fef278bd2a298fb1de1f9c
arjan <arjan@ARRU-HI>
2024-02-10 09:19:32 +0100

    First Window Layout

--- Assassins_game/Form1.Designer.cs
+++ Assassins_game/Form1.Designer.cs
@@ -28,67 +28,90 @@
         /// </summary>
         private void InitializeComponent()
         {
-            stockholm = new Button();
-            uppsala = new Button();
-            malmö = new Button();
-            göteborg = new Button();
+            login_button = new Button();
+            signup_button = new Button();
+            username_label = new Label();
+            password_label = new Label();
+            password_textbox = new TextBox();
+            username_textbox = new TextBox();
             SuspendLayout();
             // 
-            // stockholm
+            // login_button
             // 
-            stockholm.Location = new Point(142, 112);
-            stockholm.Name = "stockholm";
-            stockholm.Size = new Size(94, 29);
-            stockholm.TabIndex = 0;
-            stockholm.Text = "Stockholm";
-            stockholm.UseVisualStyleBackColor = true;
+            login_button.Location = new Point(98, 215);
+            login_button.Name = "login_button";
+            login_button.Size = new Size(94, 29);
+            login_button.TabIndex = 0;
+            login_button.Text = "Login";
+            login_button.UseVisualStyleBackColor = true;
             // 
-            // uppsala
+            // signup_button
             // 
-            uppsala.Location = new Point(142, 189);
-            uppsala.Name = "uppsala";
-            uppsala.Size = new Size(94, 29);
-            uppsala.TabIndex = 1;
-            uppsala.Text = "Uppsala";
-            uppsala.UseVisualStyleBackColor = true;
+            signup_button.Location = new Point(98, 261);
+            signup_button.Name = "signup_button";
+            signup_button.Size = new Size(94, 29);
+            signup_button.TabIndex = 1;
+            signup_button.Text = "Signup";
+            signup_button.UseVisualStyleBackColor = true;
             // 
-            // malmö
+            // username_label
             // 
-            malmö.Location = new Point(142, 275);
-            malmö.Name = "malmö";
-            malmö.Size = new Size(94, 29);
-            malmö.TabIndex = 2;
-            malmö.Text = "Malmö";
-            malmö.UseVisualStyleBackColor = true;
+            username_label.AutoSize = true;
+            username_label.Location = new Point(85, 53);
+            username_label.Name = "username_label";
+            username_label.Size = new Size(75, 20);
+            username_label.TabIndex = 2;
+            username_label.Text = "Username";
             // 
-            // göteborg
+            // password_label
             // 
-            göteborg.Location = new Point(142, 363);
-            göteborg.Name = "göteborg";
-            göteborg.Size = new Size(94, 29);
-            göteborg.TabIndex = 3;
-            göteborg.Text = "Göteborg";
-            göteborg.UseVisualStyleBackColor = true;
+            password_label.AutoSize = true;
+            password_label.Location = new Point(85, 130);
+            password_label.Name = "password_label";
+            password_label.Size = new Size(70, 20);
+            password_label.TabIndex = 3;
+            password_label.Text = "Password";
+            password_label.Click += label2_Click;
+            // 
+            // password_textbox
+            // 
+            password_textbox.Location = new Point(85, 167);
+            password_textbox.Name = "password_textbox";
+            password_textbox.Size = new Size(125, 27);
+            password_textbox.TabIndex = 4;
+            // 
+            // username_textbox
+            // 
+            username_textbox.Location = new Point(85, 86);
+            username_textbox.Name = "username_textbox";
+            username_textbox.Size = new Size(125, 27);
+            username_textbox.TabIndex = 5;
             // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(800, 450);
-            Controls.Add(göteborg);
-            Controls.Add(malmö);
-            Controls.Add(uppsala);
-            Controls.Add(stockholm);
+            ClientSize = new Size(290, 347);
+            Controls.Add(username_textbox);
+            Controls.Add(password_textbox);
+            Controls.Add(password_label);
+            Controls.Add(username_label);
+            Controls.Add(signup_button);
+            Controls.Add(login_button);
             Name = "Form1";
-            Text = "Form1";
+            Text = "Assassin_Game";
+            Load += Form1_Load;
             ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
 
-        private Button stockholm;
-        private Button uppsala;
-        private Button malmö;
-        private Button göteborg;
+        private Button login_button;
+        private Button signup_button;
+        private Label username_label;
+        private Label password_label;
+        private TextBox password_textbox;
+        private TextBox username_textbox;
     }
 }
